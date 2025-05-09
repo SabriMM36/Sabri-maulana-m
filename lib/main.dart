7d36b67bd4e8e342e192a471f7fd6a3fe4ab7d5d
unknown <sabrimaulanam36@gmail.com>
2025-05-09 07:27:52 -0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WeatherScreen(),
+    );
+  }
+}
+
+class WeatherScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('bg.jpg'),
+            fit: BoxFit.cover,
+            color: Colors.black12,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Harlem',
+              style: TextStyle(fontSize: 36, color: Colors.blue),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Tuesday, Januari 10, 2019',
+              style: TextStyle(fontSize: 18, color: Colors.blue),
+            ),
+            SizedBox(height: 30),
+            Text(
+              '15°C',
+              style: TextStyle(fontSize: 80, color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Cloudy',
+              style: TextStyle(fontSize: 24, color: Colors.blue),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '25°C / 28°C',
+              style: TextStyle(fontSize: 18, color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ uts_06tplm001_221011402996 - Copy/android/app/src/main/kotlin/com/example/uts_06tplm001_221011402996/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uts_06tplm001_221011402996
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
